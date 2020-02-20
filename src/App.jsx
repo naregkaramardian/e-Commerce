e4679b21be7c944ddf4d91841f8f5b69fc721ab0
Nareg Karamardian <nareg.Karamardian@gmail.com>
2020-02-20 16:03:56 -0500

    added signIn-SignUp page also added form-input , sign-in and custom-buton components with their style sheets

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/homepage/hompage.component"
 import ShopPage from "./pages/shop/shop.component.jsx"
 import { Switch, Route } from "react-router-dom"
 import Header from "./components/header/header.component.jsx"
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sig-in-and-sign-up.component"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   )
---
+++ src/pages/sign-in-and-sign-up/sig-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./sig-in-and-sign-up.styles.scss"
+import SignIn from "../../components/sign-in/sign-in.component.jsx"
+
+const SignInAndSignUp = () => (
+  <div className="sig-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+export default SignInAndSignUp

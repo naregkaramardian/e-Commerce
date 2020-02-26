e403cc476673305ae96893687f46704a7d65f1ea
Nareg Karamardian <nareg.Karamardian@gmail.com>
2020-02-26 16:10:47 -0500

    added a clear_item_from_cart action to the cart item component that allows the user to remove items from the cart in the checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { connect } from "react-redux"
 import { createStructuredSelector } from "reselect"
+import CheckoutItem from "../../components/checkout-item/checkout-item.component"
 
-import CheckoutItem  from "../../components/checkout-item/checkout-item.component"
 
 import {
   selectCartItems,
